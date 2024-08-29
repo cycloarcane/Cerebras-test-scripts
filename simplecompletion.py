11c40317d2636e4c5551a51ea949084ad4881998
cycloarcane <cycloarkane@gmail.com>
2024-08-28 22:57:24 -0700

    default completions test

---
+++ simplecompletion.py
@@ -0,0 +1,19 @@
+import os
+from cerebras.cloud.sdk import Cerebras
+
+client = Cerebras(
+    # This is the default and can be omitted
+    api_key=os.environ.get("CEREBRAS_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is fast inference important?",
+        }
+],
+    model="llama3.1-8b",
+)
+
+print(chat_completion)
